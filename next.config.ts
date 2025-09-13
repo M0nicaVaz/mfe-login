776f826b88ace046b9627a4eb1570f77609870c1
Monica Vaz <monica.vaz@eumedicoresidente.com.br>
2025-09-13 11:17:13 -0300

    feat: mfe config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const { NEXT_PUBLIC_HOME_URL } = process.env;
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: '/',
+  experimental: {
+    externalDir: true,
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          destination: `/:path*`,
+        },
+        {
+          source: '/home',
+          destination: `${NEXT_PUBLIC_HOME_URL}/home`,
+        },
+        {
+          source: '/home/:path*',
+          destination: `${NEXT_PUBLIC_HOME_URL}/home/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
